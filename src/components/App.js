2224bacbe6f95825e4e260f73432cd206e5fcd5e
sec05 <58738123+sec05@users.noreply.github.com>
2020-07-11 18:38:26 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Title">
+      <h1 id="mTitle">
+        My Capstone Project
+       
+      </h1>
+      <h5 >
+        by, Spencer Evans-Cole
+      </h5>
+      </div>
+    </div>
+  );
+}
+
+export default App;
